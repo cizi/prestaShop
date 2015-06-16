406e01331c9497c6c3f42154c261c2901d1e175c
cizi <cizi@email.cz>
2015-06-16 10:35:46 +0200

    Special rules for layers switching added

--- themes/pos_accessories3/js/man_engine.js
+++ themes/pos_accessories3/js/man_engine.js
@@ -130,3 +130,30 @@ function create_element_id(item)
 
     return element_id;
 }
+
+/*
+Funkce prehazuje vrsvy podle spec. pravidel
+10 x 50
+10 x 30
+50 x 30
+20 x 30
+*/
+function layersSpecialRules(currentLayer, layerOn)
+{
+  switch(currentLayer | layerOn)
+  {
+      case 10 | 50:
+      case 50 | 10:
+      case 10 | 30:
+      case 30 | 10:
+      case 50 | 30:
+      case 30 | 50:
+      case 20 | 30:
+      case 30 | 20:
+        return true;
+        break;
+      default:
+        return false;
+        break;
+  }
+}
